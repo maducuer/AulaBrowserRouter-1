297190b7c5db3d96497bb79c9be865f73adcd61f
fvfr <fevef@dnwahjsdn>
2024-08-12 17:24:20 -0400

    12/08

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+export default function Header(){
+    return
+    <header>
+
+        <Link to= "/home">
+        <p>Home</p>
+        </Link>
+
+        <Link to= "/produtos">
+        <p>Produtos</p>
+        </Link>
+
+        <Link to= "/ofertas">
+        <p>Ofertas</p>
+        </Link>
+
+    </header>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRauter , Routes } from 'react-router-dom'
+import Home from "./home"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRauter>
+    <Routes>
+    <Route path="/home" element={<Home/>}/>
+    <Route path="/produtos" element={<Produtos/>}/>
+    <Route path="/ofertas" element={<Ofertas/>}/>
+    </Routes>
+    </BrowserRauter>
   </React.StrictMode>,
 )
 
