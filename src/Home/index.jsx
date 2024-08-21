8aaf5c881bec1a8a9e2b4f3a126ed96c5dcf90da
Manuh08 <maducuer@gmail.com>
2024-08-21 14:28:19 -0400

    terminar
    aaaaa

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,6 +1,4 @@
 import {useState} from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './Home'
-import Oferta from './Oferta'
+import Oferta from './Ofertas'
+import Produtos from './Produtos'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 <BrowserRouter>
     <Routes>
-    <Route path="/home" element={<Home/>}/>
-    <Route path="/produtos" element={<Produtos/>}/>
-    <Route path="/ofertas" element={<Oferta/>}/>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/produtos' element={<Produtos/>}/>
+    <Route path='/ofertas' element={<Ofertas/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
